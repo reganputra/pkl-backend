937696bd0c6ec9837a8bcd407301ae01b1fb830a
reganputra <ramadhanregan334@gmail.com>
2025-04-21 06:04:10 +0700

    feat: addressing cors

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors('http://localhost:5173/'));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
